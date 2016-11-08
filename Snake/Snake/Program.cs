5b28ac72d9133c1c0a59a3fc4afd5bcec2470bd6
Vovantenok <student@list.ru>
2016-11-09 02:19:18 +0300

    Отрисовка змейки

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -24,10 +24,11 @@ namespace Snake
 
             //отрисовка точек
             Point p = new Point(4, 5, '*');
-            p.Draw();
-
-
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
             Console.ReadLine();
+
+
         }
 
     }
