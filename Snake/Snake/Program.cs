3a93bb09d4444d226d7a5f1c7e906700309dc5c3
Vovantenok <student@list.ru>
2016-11-10 03:11:23 +0300

    Console.SetBufferSize(80, 25); - не работает(((

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,20 +13,16 @@ namespace Snake
         {
             //Console.SetBufferSize(80, 25);
 
-            //отрисовка рамочки
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(1, 23, 0, '+');
-            VerticalLine rightLine = new VerticalLine(1, 23, 78, '+');
-            upLine.Drow();
-            downLine.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
+            
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+            
 
             //отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Drow();
+            snake.Draw();
 
             FoodCreator foodCreator = new FoodCreator(77, 23, '$');
             Point food = foodCreator.CreateFood();
@@ -34,6 +30,10 @@ namespace Snake
 
             while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
